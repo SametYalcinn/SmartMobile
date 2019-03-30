babc2a924b77e2231176e4c9e47dbc507e56daa1
Samet Yalcin <syalcin_@hotmail.com>
2019-03-30 16:16:34 +0100

    IOS bootcamp Dice App & Playground

---
+++ IOS Bootcamp/Variables, Contstants & Data Types.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var myAge : Int = 32
+
+myAge = 33
+
+let myName : String = "Samet"
+
+
+let myAgeInTenYears = myAge + 10
+
+let myDetails = "\(myName), \(myAge)"
+
+let wholeNumbers : Int = 12
+let text : String = "abc"
+
+let booleans : Bool = true
+
+let floatingPointNumber : Float = 1.3
+
+let double : Double = 3.141553767356
